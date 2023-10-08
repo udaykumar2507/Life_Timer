2b1407909ebc7fc5759304ab6b2dd34eb9afa2b7
Uday Kumar Maurya <122080383+udaykumar2507@users.noreply.github.com>
2023-10-08 20:30:36 +0530

    Update index.js
    Made Some changes in the Calculation Part

--- index.js
+++ index.js
@@ -34,12 +34,13 @@ const toggleDateOfBirthSelector = () => {
 const updateAge = () => {
   const currentDate = new Date();
   const dateDiff = currentDate - dateOfBirth;
-  const year = Math.floor(dateDiff / (1000 * 60 * 60 * 24 * 365));
-  const month = Math.floor((dateDiff / (1000 * 60 * 60 * 24 * 365)) % 12);
-  const day = Math.floor(dateDiff / (1000 * 60 * 60 * 24)) % 30;
-  const hour = Math.floor(dateDiff / (1000 * 60 * 60)) % 24;
-  const minute = Math.floor(dateDiff / (1000 * 60)) % 60;
-  const second = Math.floor(dateDiff / 1000) % 60;
+  const year = Math.floor(dateDiff / (1000 * 60 * 60 * 24 * 365.25));
+  const remainingtime=dateDiff-year*(1000*60*60*24*365.25)
+  const month = Math.floor((remainingtime / (1000 * 60 * 60 * 24 *30.44)));
+  const day = Math.floor((remainingtime % (1000 * 60 * 60 * 24*30.44))/(1000*60*60*24));
+  const hour = Math.floor((remainingtime% (1000 * 60 * 60*24)) / (1000*60*60));
+  const minute = Math.floor((remainingtime% (1000 * 60*60)) / (1000*60));
+  const second = Math.floor((remainingtime% (1000*60)) / 1000);
 
   yearEl.innerHTML = makeTwoDigitNumber(year);
   monthEl.innerHTML = makeTwoDigitNumber(month);
